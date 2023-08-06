dc7a0a913b5e4ea2cea3cb7cb9dfded0f77f5814
Chris Abey <chris.abey587@gmail.com>
2023-08-06 00:51:28 -0400

    added text

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi, I'm Chris.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
